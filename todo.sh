54442966be3f47aab870c84f0fa5bc5421a55747
Trigger-BOI <tuurlammes1@gmail.com>
2025-01-06 11:38:35 +0100

    added colors to cat_todo

--- todo.sh
+++ todo.sh
@@ -7,6 +7,14 @@ FILE_TYPE="md"
 EDITOR="nvim"
 # ---------------------------
 
+# COLORS
+BOLD_YELLOW='\033[1;33m'
+ITALIC_GREEN='\033[3;32m'
+RESET='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
 function list_todos() {
     echo "Available todo lists:"
     for file in "$TODO_DIR"/*.$FILE_TYPE; do
@@ -26,7 +34,19 @@ function edit_todo() {
 function cat_todo() {
     local todo_file="$TODO_DIR/$1.$FILE_TYPE"
     if [[ -f "$todo_file" ]]; then
-        cat "$todo_file"
+        while IFS= read -r line; do
+            # Replace **text** with bold yellow text
+            line=$(echo "$line" | sed -E "s/\*\*([^*]+)\*\*/\\${BOLD_YELLOW}\1\\${RESET}/g")
+            # Replace *text* with italic green text
+            line=$(echo "$line" | sed -E "s/\*([^*]+)\*/\\${ITALIC_GREEN}\1\\${RESET}/g")
+            # Replace # text with red text
+            line=$(echo "$line" | sed -E "s/^# (.+)/\\${RED}\1\\${RESET}/g")
+            # Replace ## text with yellow text
+            line=$(echo "$line" | sed -E "s/^## (.+)/\\${YELLOW}\1\\${RESET}/g")
+            # Replace ### text with green text
+            line=$(echo "$line" | sed -E "s/^### (.+)/\\${GREEN}\1\\${RESET}/g")
+            printf "%b\n" "$line"
+        done < "$todo_file"
     else
         echo "Todo list '$1' does not exist."
     fi
